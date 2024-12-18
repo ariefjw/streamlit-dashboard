7f8fa903a1104ba8fa7694fed68df11dcbf3f08c
Arief JW <ariefjoko78@gmail.com>
2024-12-18 16:49:10 +0700

    memperbaiki struktur AI

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ import plotly.graph_objects as go
 import psycopg2
 from groq import Groq
 from PIL import Image
+from typing import Generator
 
 #setup groq
 client = Groq(
     api_key= st.secrets["GROQ_API_KEY"],
 )
 
+df = pd.read_csv('data_for_database.csv')
 #page configuration
 st.set_page_config(
     page_title="Dashboard",
@@ -55,7 +57,6 @@ with page_title:
   st.markdown(title, unsafe_allow_html=True)
 st.markdown('---')
 
-pilihan = st.selectbox('Pilih kolom:',('Age','Weight','Height','ShootingTotal'))
 created_by, chart1, chart2 = st.columns([0.15,0.4,0.4])
 with created_by:
   st.markdown(creator, unsafe_allow_html=True)
@@ -65,19 +66,56 @@ with chart1:
 #   st.plotly_chart(fig_trend_sales, use_container_width=True)
 
 
-__ , chat_ai = st.columns([0.15, 0.85])
+
+
+
+
+
+_1_ , chat_ai = st.columns([0.15, 0.85])
 
 
 with chat_ai:
-  chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": f" Analisa berdasarkan data {data}",
-        }
-    ],
-    model="llama3-8b-8192",
-  )
+  def chat_respon(text) -> Generator[str, None, None]:
+    """
+    Memberikan respon kepada user dari API respon groq
+    """
+    for word in text:
+        if word.choices[0].delta.content:
+            yield word.choices[0].delta.content
+ 
+  if "messages" not in st.session_state:
+      st.session_state.messages = []
   
-  st.write(f"{chat_completion.choices[0].message.content}")
+  if prompt := st.chat_input("Anda ingin bertanya?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user", avatar='🐣'):
+      st.markdown(prompt)
+    
+    try:
+        chat_completion = client.chat.completions.create(
+            model="llama3-8b-8192",
+            messages=[
+                {
+                    "role": m["role"],
+                    "content": f"{m["content"]}, berikan jawaban berkaitan dengan {df}. Jawab dengan bahasa sesuai dengan bahasa di : ({m["content"]})"
+                }
+                for m in st.session_state.messages
+            ],
+            stream=True
+        )
+        with st.chat_message("assistant", avatar='👩‍🏫'):
+            prompt_respon = chat_respon(chat_completion)
+            full_response = st.write_stream(prompt_respon)
+    except Exception as e:
+        st.error(e, icon="⚠️")
+
+     
+    if isinstance(full_response, str):
+        st.session_state.messages.append(
+            {"role": "assistant", "content": full_response})
+    else:
+        response_convert = "\n".join(str(item) for item in full_response)
+        st.session_state.messages.append(
+            {"role": "assistant", "content": response_convert})
+
 
